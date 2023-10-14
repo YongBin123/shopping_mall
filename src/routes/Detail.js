19fb3f053482e1bc5184c555ef66d172b9d0046b
YongBin123 <134280533+YongBin123@users.noreply.github.com>
2023-10-14 17:29:12 +0900

    [Feat] Lifecycle과 useEffect 실습 코드 추가
    리액트 영상의 Lifecycle과 useEffect 실습한 부분 코드입니다!

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
 function Detail(props) {
 
+  let [count, setCount] = useState(0)
   let {id} = useParams();
+  let [alert, setAlert] = useState(true)
 
-    return <div className="container">
+  useEffect(()=>{
+    setTimeout(()=>{ setAlert(false) }, 2000)
+  }, [count])
+
+    return (
+    <div className="container">
+      {
+        alert == true
+        ? <div className="alert alert-warning">
+          2초이내 구매시 할인
+          </div>
+        : null
+      }
+      
+      {count}
+      <button onClick={()=>{ setCount(count+1) }}>버튼</button>
     <div className="row">
       <div className="col-md-6">
         <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
@@ -17,6 +35,6 @@ function Detail(props) {
       </div>
     </div>
   </div> 
-}
+)}
 
 export default Detail;
